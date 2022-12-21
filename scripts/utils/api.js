3d9513cac93712c7e1a3060a757c94959437f871
PΩB <100569120+P0B0CK@users.noreply.github.com>
2022-12-21 18:23:04 +0100

    Update No 07 :: Suppression .JS
    - Suppression de fichier .JS inutiles

--- scripts/utils/api.js
+++
@@ -1,9 +0,0 @@
-export async function api() {
-    let recipes;
-
-    await fetch("../../datas/recipes.json")
-        .then((res) => res.json())
-        .then((data) => console.log(data));
-
-        console.log(recipes);
-}
