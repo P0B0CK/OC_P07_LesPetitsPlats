71574dd5beaeb41c4a241102723f9a193dca8794
PΩB <100569120+P0B0CK@users.noreply.github.com>
2022-12-22 21:52:28 +0100

    Update No 09 :: Array ingredient

--- scripts/factories/recipeCard.js
+++ scripts/factories/recipeCard.js
@@ -19,11 +19,28 @@ export function recipeCard(recipes) {
                                             <div class="recipe-duration"><img src="assets/img/icon-time.svg" alt="Icone Temps de préparation"><p>${time} min</p></div>
                                             </div>
                                                 <div class="details-description">
-                                                    <div class="recipe-necessary">${ingredients}</div>
+                                                    <div class="recipe-necessary"><ul>${ingredients.map(elt => recipeIngredient(elt))}</ul></div>
                                                     <div class="recipe-describe">${description}</div>
                                                 </div>
                                             </div>
                         `;
+    
+    function recipeIngredient(elt){
+        let datas = ``;
+        if(elt.unit && elt.quantity) {
+            datas = `<li>${elt.ingredient} : ${elt.quantity} ${elt.unit}</li>`;
+        } else if (elt.quantity &&  !elt.unit) {
+            datas = `<li>${elt.ingredient} : ${elt.quantity}</li>`;
+        } else {
+            datas = `<li>${elt.ingredient}</li>`;
+        }
+        return datas;
+    }
+
+    function createIngredientType() {
+        let tabIngredient = [];
+        ingredients.map( elt => tabIngredient.push(elt.ingredient)) 
+    }
 }
 
 
